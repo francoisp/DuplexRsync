990df62cce0944763516c505de39b677043bd540
francois payette <francoisp@netmosphere.net>
2019-03-20 20:21:06 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# DuplexRsync
+
+I often find myself editing quite a few files on remote hosts; for anything non-trivial I like using local-running tools such as Sublime. I've used [rsub](https://github.com/henrikpersson/rsub), it's very nice and lightweight. Sometimes(often) the light editing turns heavier and more and more files are worked on.
+
+When things keep getting heavier, I then use [sshfs](https://github.com/osxfuse/osxfuse/wiki/SSHFS) to mount a remote directory and fuse it to the local fs. This usually works ok, but for some types of workflows such as sublime projects with a lot of files in subfolders (node_modules? --sometimes this one starts to feel like a slackware distro) it is inadequate. Search becomes extra slow. THe project tree spins and spins and spins; features that have become automatisms are unworkable.
+
+So DuplexRsync is a simple and pretty sweet (although only lightly tested) solution based on fswatch and rsync. It's a single file you'll put in your local directory that will maintain (DropBox|GoogleDrive)-style 2-way sync between the current directory and a remote directory via SSH. This has the advantage to work fine when offline. This bash script is a bit macOSX-centric because that's what I use locally, please feel free to adapt. By default the script excludes nodes_modules and all folders that start with a period. (.git etc)
+
+
+##  Setup
+
+on your remote machine you'll need fswatch:
+
+
+    sudo add-apt-repository ppa:hadret/fswatch
+    sudo apt-get update
+    sudo apt-get install -y fswatch
+
+on your local machine you'll need brew, that's it. This script will install the other required components (socat fswatch and gnu-getopt)
+
+    chmod u+x duplexRsync.sh
+    ./duplexRsync.sh --remoteHost user@192.168.0.2
+
+Please Note: A few hidden files are created to maintain the 2-way sync, they all start by .____*. The remote directory will be straight off the home of your remote user's home; there's an optional --remoteParent if you need to change that.
+
+
+License: MIT
---
+++ duplexRsync.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+# REQUIREMENT we need fswatch on both ends, run this to get it on ubuntu1604
+#sudo add-apt-repository ppa:hadret/fswatch
+#sudo apt-get update
+#sudo apt-get install fswatch
+printHelp(){
+  echo "USAGE: duplexRsync --remoteHost
+  DuplexRsync requires fswatch on both ends, this tries to install it locally using brew(required).
+    on the remote end run:
+    sudo add-apt-repository ppa:hadret/fswatch
+    sudo apt-get update
+    sudo apt-get install fswatch
+  you need to specify a --remoteHost such as user@192.168.0.2.
+  You can also optionaly specify a --remoteParent that contains/will contain the remmoteDir"
+}
+
+if [ -z $(which brew) ];
+then
+  printHelp;
+  exit
+fi
+
+# this is for macosx, we also need socat to create a socket to remote trigger rsync
+brew install socat fswatch gnu-getopt
+
+if ! options=$(/usr/local/Cellar/gnu-getopt/1.1.6/bin/getopt -u -o hr:p: -l help,remoteHost:,remoteParent: -- "$@")
+then
+    # something went wrong, getopt will put out an error message for us
+    exit 1
+fi
+
+
+set -- $options
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+    # for options with required arguments, an additional shift is required
+    -h|--help ) printHelp; exit; shift;;
+    -r|--remoteHost ) remoteHost=$2; shift;;
+    -p|--remoteParent ) remoteParent=$2; shift;;
+
+    --) shift; break;;
+    #(-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
+    (*) break;;
+    esac
+    shift
+done
+
+remoteDir=${PWD##*/}
+remoteDir="$remoteParent$remoteDir"
+
+# if we have the ssh tunnel running this will match and we kill it
+pkill -f rsyncSignal
+# if we have a lingering socat kill it
+pkill -f TCP-LISTEN:9091
+
+
+
+# we'll need to ssh without pass - use public key crypto to ssh into remote end,  rsync needs this
+#we are copying our pubkey to ssh in without prompt
+cat ~/.ssh/id_rsa.pub | ssh "$remoteHost"  'mkdir .ssh;pubkey=$(cat); if grep -q "$pubkey" ".ssh/authorized_keys"; then echo "puublic key for this user already present"; else echo $pubkey >> .ssh/authorized_keys;fi'
+
+fswatchPath=$(ssh "$remoteHost" 'which fswatch')
+if [ -z "$fswatchPath" ];
+then
+  printHelp;
+  exit;
+fi
+#we dump to a remote file the fswatch command that allows local running socat to get a signal of a remote change
+echo 'fswatch -e "node_modules" -o . | while read f; do echo 1 | netcat localhost 9091; done' | ssh $remoteHost  "mkdir -p $remoteDir; cd $remoteDir; cat > .____rsyncSignal.sh"
+
+#if [ ! -f .____sentinel ];
+#then
+# we always create a new sentinel file
+echo '0' > .____sentinel
+#fi
+
+
+#one liner script to increment our sentinel
+echo 'sentval=$(cat .____sentinel);sentval=$((sentval+1));echo $sentval > .____sentinel;' > ./.____sentinelIncrement.sh
+chmod a+x ./.____sentinelIncrement.sh
+#create socket to listen for remote changes
+socat TCP-LISTEN:9091,fork EXEC:"./.____sentinelIncrement.sh" > /dev/null 2>&1 &
+#socat TCP-LISTEN:9091,fork EXEC:"./increment_sentinel.sh" > /dev/null 2>&1 &
+# cannot seem to execute bash command form fork EXEC if it would be possible we'd have one less file
+#socat TCP-LISTEN:9091,fork EXEC:"/bin/bash -c 'sentval=$(cat .____sentinel);sentval=$((sentval+1));echo $sentval'" > /dev/null 2>&1 &
+
+function duplex_rsync() {
+    # kill the remote fswatch while we sync
+    ssh $remoteHost 'pkill -f rsyncSignal'
+    # also kill the tunnel
+    pkill -f rsyncSignal
+
+    # order matters; if we got a remote trigger we'll process remote as src first to prevent restoring files that might have just been deleted
+    if [ "$trigger" = "remote" ];
+    then
+      rsync -auzP --exclude ".*/" --exclude ".____*"  --exclude ".____sentinel" --exclude "node_modules" --delete "$remoteHost:$remoteDir/" .;
+      rsync -auzP --exclude ".*/" --exclude ".____*" --exclude ".____sentinel"  --exclude "node_modules" --delete . "$remoteHost:$remoteDir";
+    else # local as src first
+      rsync -auzP --exclude ".*/" --exclude ".____*" --exclude ".____sentinel"  --exclude "node_modules" --delete . "$remoteHost:$remoteDir";
+      rsync -auzP --exclude ".*/" --exclude ".____*"  --exclude ".____sentinel" --exclude "node_modules" --delete "$remoteHost:$remoteDir/" .;
+    fi;
+
+    ssh  -R localhost:9091:127.0.0.1:9091 $remoteHost "cd $remoteDir; bash .____rsyncSignal.sh"&
+    #tunnelPid="$!"
+    # echo "tunnelPid:$tunnelPid"
+}
+
+lastSentinel=$(cat .____sentinel);
+
+# we always start from the local dir
+trigger=local;
+# do a trial run to see if we'd delete files on the remote end
+wouldDeleteCount=$(rsync -anuzP --exclude ".*/" --exclude ".____*"  --exclude ".____sentinel" --exclude "node_modules" --delete . $remoteHost:$remoteDir/ | grep deleting | wc -l);
+wouldDeleteCount="$(echo -e "${wouldDeleteCount}" | tr -d '[:space:]')"
+
+wouldDeleteRemoteFiles=$(rsync -anuzP --exclude ".*/" --exclude ".____*"  --exclude ".____sentinel" --exclude "node_modules" --delete . $remoteHost:$remoteDir/ | grep deleting);
+if [ ! -z "$wouldDeleteRemoteFiles" ];
+then
+  echo "WOULD delete count: $wouldDeleteCount"
+  echo "$wouldDeleteRemoteFiles"
+  #echo "THE DESTINATION DIR CONTAINS $wouldDeleteCount"
+  unset destroyAhead
+  while ! [[ "$destroyAhead" =~ ^(destroy|restore|abort)$ ]]
+  do
+
+    if [ "$wouldDeleteCount" -gt 5 ]
+    then
+      major=" ----MAJOR----- ";
+    fi
+    echo "ATTENTION $major DESTRUCTION  AHEAD: There is/are $wouldDeleteCount file(s) present in the remote folder that are not present locally. Could the remote folder be totally unrelated? Would you like to restore them locally(restore),Sync and destroy(destroy) or abort?(restore/destroy/abort)"
+    read destroyAhead
+  done
+  if [ "$destroyAhead" = "abort" ];
+  then
+    exit;
+  elif [ "$destroyAhead" = "restore" ];
+  then
+    # sync from remote without delete
+    rsync -auzP --exclude ".*/" --exclude ".____*"  --exclude ".____sentinel" --exclude "node_modules" "$remoteHost:$remoteDir/" .;
+  fi
+fi;
+
+duplex_rsync;fswatch -r -o . | while read f;
+  do
+    sentinel=$(cat .____sentinel);
+    sentinelInc=$((sentinel-lastSentinel));
+    # if the change is remote(incremented ____sentinel) lets slow down and wait to gobble multiple events
+    if [ $sentinelInc -gt 0 ]
+    then
+      trigger=remote;
+      duplex_rsync;
+      sleep 3;
+    else
+      trigger=local;
+      duplex_rsync;
+    fi
+    lastSentinel=$sentinel;
+  done;
